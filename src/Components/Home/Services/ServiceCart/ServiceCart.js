7869fc63bda6d6beddfa42c0c904cef2415f9e65
nurboss <nurmohammod993@gmail.com>
2021-10-31 23:37:10 +0600

    end

--- src/Components/Home/Services/ServiceCart/ServiceCart.js
+++ src/Components/Home/Services/ServiceCart/ServiceCart.js
@@ -5,7 +5,7 @@ import './ServiceCart.css';
 const ServiceCart = (props) => {
     const {title, description, img, _id} = props.cartData;
     return (
-        <div className="content col-lg-4 col-md-6 col-md-12 ">
+        <div className="content col-lg-4 col-md-6 col-md-12 mb-5">
                 <div className="cart">
                     <div className="cart-img">
                         <img src={img} alt="" />
@@ -20,32 +20,6 @@ const ServiceCart = (props) => {
                             </Link>                      
                         </div>
                 </div>
-
-
-
-
-
-
-
-
-                {/* <div className="box">
-                <div>
-                        <div className="img-box">
-                            <img src={img} alt="" />
-                        </div>
-                    <div className="text">
-                          <div className="text-box">
-                            <h4 className="fw-bold">{title}</h4>
-                            <p>{description}</p>
-                            </div>
-                          </div>
-                </div>
-                        <div className=" my-3 d-flex             justify-content-between"> 
-                            <Link to={`/details/${_id}`}>
-                            <button className="button">Details</button>
-                            </Link>                      
-                        </div>
-                </div> */}
             </div>
     );
 };
